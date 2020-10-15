7306d43e7002c536fd1fc06a6749bcd3a0f31621
Kailashh <bkailashh1996cse@gmail.com>
2020-10-15 20:46:25 +0530

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -31,7 +31,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return _context.Users.FindAsync(id);
+            return await _context.Users.FindAsync(id);
 
             
 
