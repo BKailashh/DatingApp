4c317eeb7abbb0287ecaddb481967712e504073b
Kailashh <bkailashh1996cse@gmail.com>
2020-10-31 12:43:03 +0530

    End of section 6

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  registerMode=false;
+  users:any;
+  constructor(private Http : HttpClient) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  registerToggle()
+  {
+    this.registerMode=!this.registerMode;
+  }
+
+  getUsers()
+  {
+    this.Http.get('https://localhost:5001/api/users').subscribe(users=>this.users=users);
+
+  }
+
+  cancelRegisterMode(event:boolean)
+  {
+    this.registerMode=event;
+
+  }
+
+}
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Console } from 'console';
+import { ToastrService } from 'ngx-toastr';
+import { error } from 'protractor';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model:any={};
+  loggedIn:boolean;
+  constructor(public accountService: AccountService,private router:Router,private toastr:ToastrService) { 
+    console.log("navc");
+  }
+
+  ngOnInit(): void {
+    console.log("nav");
+
+  }
+
+  login() 
+  {
+    console.log("login");
+    this.accountService.login(this.model).subscribe(
+      Response=>{
+        console.log(Response);
+        this.router.navigateByUrl('/members');
+        },
+        error=>
+        {
+          console.log(error);
+          this.toastr.error(error.error);
+        }
+        );
+    
+
+  }
+
+  logout()
+  {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+
+  }
+
+  
+
+}
