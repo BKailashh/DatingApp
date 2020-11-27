7fc70e95092859bf2fdf1b2f65468b3145ca920e
BKailashh <bkailashh1996cse@gmail.com>
2020-11-27 15:45:31 +0530

    End of section 10

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
+  canDeactivate(component: MemberEditComponent): boolean  
+  {
+    if(component.editForm.dirty)
+    {
+      console.log("check");
+      return confirm("Are you sure you want to continue? Any unsaved changes will be lost if you do so.")
+    }
+
+    return true;
+
+    
+    
+  }
+  
+}
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import {map} from 'rxjs/operators';
 
 
 @Injectable({
@@ -9,16 +11,61 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl=environment.apiUrl;
+  members:Member[]=[];
 
   constructor(private http:HttpClient) { }
 
+  //old way
+  /*getMembers()
+  {
+
+    return this.http.get<Member[]>(this.baseUrl+'users');
+  }*/
+
+  //new way
   getMembers()
   {
-    return this.http.get<Member[]>(this.baseUrl+'users');
+    if(this.members.length>0) return of(this.members);
+
+    return this.http.get<Member[]>(this.baseUrl+'users').pipe(
+      map(members=>{
+        this.members=members;
+        return members;
+      }
+      )
+    )
   }
 
-  getMember(username:string)
+  //old way
+  /*getMember(username: string)
   {
     return this.http.get<Member>(this.baseUrl+'users/'+username);
+  }*/
+  
+  //new way
+  getMember(username:string)
+  {
+    const member=this.members.find(x=>x.username===username);
+    if(member!==undefined) return of(member);
+    return this.http.get<Member>(this.baseUrl+'users/'+username);
+  }
+
+  //old way
+  /*updateMember(member: Member)
+  {
+    return this.http.put(this.baseUrl+'users',member);
+  }*/
+ 
+  //new way
+  updateMember(member: Member)
+  {
+    return this.http.put(this.baseUrl+'users',member).pipe(
+      map(
+        ()=>{
+          const index=this.members.indexOf(member);
+          this.members[index]=member;
+        }
+      )
+    )
   }
 }
